cf57cab024e1135d35f5e5c5d337cc5fb4dcd58f
dhine <dhine@THORFINN>
2024-10-15 12:10:11 +0530

    Add project files.

---
+++ Reg Form/Models/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI.WebControls;
+
+namespace Reg_Form.Models
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        [RegularExpression("@",ErrorMessage ="You are missing an expression")]
+        public string Email { get; set; }
+ 
+        [Required(ErrorMessage = "Enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Enter the age")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Enter the password")]
+        public string password {  get; set; }
+
+        [Compare("Password",ErrorMessage = "They are not the same")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
